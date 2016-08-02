7c1407247f2269eb2ce3a1706c4d9d3dd6689eab
joblolin95 <blaineo_95@yahoo.com>
2016-08-02 10:34:29 -0400

    Add button on click listeners

--- app/src/main/java/com/example/blain/mypermapp/MainActivity.java
+++ app/src/main/java/com/example/blain/mypermapp/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.blain.mypermapp;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button reqContactPermBtn, reqCameraPermBtn, checkContactPermBtn, checkCameraPermBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkContactPermBtn = (Button) findViewById(R.id.checkContactsPermBtn);
+        reqContactPermBtn = (Button) findViewById(R.id.reqContactsPermBtn);
+        checkCameraPermBtn = (Button) findViewById(R.id.checkCameraPermBtn);
+        reqCameraPermBtn = (Button) findViewById(R.id.reqCameraPermBtn);
+
+        checkContactPermBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        reqContactPermBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        checkCameraPermBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        reqCameraPermBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 }
